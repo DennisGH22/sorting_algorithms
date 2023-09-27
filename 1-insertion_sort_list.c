fd5de09e5f0efab59b1382949b959e95a60aa17a
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-27 11:33:15 +0200

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,8 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sort a doubly linked list in ascending order.
+ * insertion_sort_list - Sort a doubly linked list
+ * in ascending order.
  *
  * @list: Pointer to a pointer to the head of the list.
 */
