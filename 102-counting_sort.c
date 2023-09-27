5048d5d8be8a75f7def17395393ff0a61b7ff7e1
Tebogo Ndaba <dennistebogo@gmail.com>
2023-09-27 22:08:46 +0200

    Counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,68 +1,59 @@
 #include "sort.h"
 
 /**
- * counting_sort - Sorts an array of integers in ascending order
- * using the Counting sort algorithm.
+ * counting_sort - Sorts an array of integers using Counting Sort.
  *
- * @array: Pointer to the array of integers to be sorted.
- * @size: Size of the array.
- */
+ * @array: The array of integers to be sorted.
+ * @size: The size of the array.
+*/
 void counting_sort(int *array, size_t size)
 {
-    int max, min, i;
-    int *count, *output;
+    int i, max, *output, *count;
+    size_t j;
 
     if (array == NULL || size < 2)
         return;
 
     max = array[0];
-    min = array[0];
-    for (i = 1; i < (int)size; i++)
+    for (j = 1; j < size; j++)
     {
-        if (array[i] > max)
-            max = array[i];
-        if (array[i] < min)
-            min = array[i];
+        if (array[j] > max)
+            max = array[j];
     }
 
-    count = malloc(sizeof(int) * (max - min + 1));
+    count = malloc((max + 1) * sizeof(int));
     if (count == NULL)
         return;
 
-    for (i = 0; i <= max - min; i++)
+    for (i = 0; i <= max; i++)
         count[i] = 0;
 
-    for (i = 0; i < (int)size; i++)
-        count[array[i] - min]++;
+    for (j = 0; j < size; j++)
+        count[array[j]]++;
 
-    output = malloc(sizeof(int) * size);
+    for (i = 1; i <= max; i++)
+        count[i] += count[i - 1];
+
+    output = malloc(size * sizeof(int));
     if (output == NULL)
     {
         free(count);
         return;
     }
 
-    for (i = 1; i <= max - min; i++)
-        count[i] += count[i - 1];
-
     for (i = size - 1; i >= 0; i--)
     {
-        output[count[array[i] - min] - 1] = array[i];
-        count[array[i] - min]--;
+        output[count[array[i]] - 1] = array[i];
+        count[array[i]]--;
     }
 
-    for (i = 0; i < (int)size; i++)
-        array[i] = output[i];
+    for (j = 0; j < size; j++)
+        array[j] = output[j];
 
-    printf("Counting array: ");
-    for (i = 0; i <= max - min; i++)
-    {
-        if (i < max - min)
-            printf("%d, ", count[i]);
-        else
-            printf("%d\n", count[i]);
-    }
+    printf("Array after counting:\n");
+    for (i = 0; i <= max; i++)
+        printf("%d: %d\n", i, count[i]);
 
-    free(count);
     free(output);
+    free(count);
 }
