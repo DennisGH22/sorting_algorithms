2c50634f0591d80f3cd2a4902e2b7774c771f5fb
Tebogo Ndaba <dennistebogo@gmail.com>
2023-09-27 23:13:45 +0200

    Merge sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -10,40 +10,40 @@
 */
 void merge(int *array, int *left, int *right, size_t size)
 {
-    int *temp;
-    size_t i = 0, j = 0, k = 0;
+	int *temp;
+	size_t i = 0, j = 0, k = 0;
 
-    temp = malloc(sizeof(int) * size);
-    if (temp == NULL)
-        return;
+	temp = malloc(sizeof(int) * size);
+	if (temp == NULL)
+		return;
 
-    printf("Merging...\n");
-    printf("[left]: ");
-    print_array(left, size / 2);
-    printf("[right]: ");
-    print_array(right, size - (size / 2));
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(left, size / 2);
+	printf("[right]: ");
+	print_array(right, size - (size / 2));
 
-    while (i < size / 2 && j < size - (size / 2))
-    {
-        if (left[i] < right[j])
-            temp[k++] = left[i++];
-        else
-            temp[k++] = right[j++];
-    }
+	while (i < size / 2 && j < size - (size / 2))
+	{
+		if (left[i] < right[j])
+			temp[k++] = left[i++];
+		else
+			temp[k++] = right[j++];
+	}
 
-    while (i < size / 2)
-        temp[k++] = left[i++];
+	while (i < size / 2)
+		temp[k++] = left[i++];
 
-    while (j < size - (size / 2))
-        temp[k++] = right[j++];
+	while (j < size - (size / 2))
+		temp[k++] = right[j++];
 
-    for (i = 0; i < size; i++)
-        array[i] = temp[i];
+	for (i = 0; i < size; i++)
+		array[i] = temp[i];
 
-    printf("[Done]: ");
-    print_array(array, size);
+	printf("[Done]: ");
+	print_array(array, size);
 
-    free(temp);
+	free(temp);
 }
 
 /**
@@ -54,21 +54,21 @@ void merge(int *array, int *left, int *right, size_t size)
 */
 void merge_sort(int *array, size_t size)
 {
-    size_t middle;
-    int *left, *right;
+	size_t middle;
+	int *left, *right;
 
-    if (array == NULL || size <= 1)
-        return;
+	if (array == NULL || size <= 1)
+		return;
 
-    if (size > 1)
-    {
-        middle = size / 2;
-        left = array;
-        right = array + middle;
+	if (size > 1)
+	{
+		middle = size / 2;
+		left = array;
+		right = array + middle;
 
-        merge_sort(left, middle);
-        merge_sort(right, size - middle);
+		merge_sort(left, middle);
+		merge_sort(right, size - middle);
 
-        merge(array, left, right, size);
-    }
+		merge(array, left, right, size);
+	}
 }
