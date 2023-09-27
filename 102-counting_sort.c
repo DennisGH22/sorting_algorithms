8ac906376c638d1e364692310e27ea6c8f04cfbe
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-27 20:38:08 +0200

    Counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -8,57 +8,59 @@
 */
 void counting_sort(int *array, size_t size)
 {
-    int max, i;
-    int *count, *output;
+	int max, i;
+	int *count, *output;
 
-    if (array == NULL || size < 2)
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 1; i < (int)size; i++)
+    {
+        if (array[i] != array[i - 1])
+            break;
+    }
+
+    if (i == (int)size)
         return;
 
-    max = array[0];
-    for (i = 1; i < (int)size; i++)
-    {
-        if (array[i] > max)
-            max = array[i];
-    }
+	count = malloc(sizeof(int) * (max + 1));
+	if (count == NULL)
+		return;
 
-    count = malloc(sizeof(int) * (max + 1));
-    if (count == NULL)
-        return;
+	for (i = 0; i <= max; i++)
+		count[i] = 0;
 
-    for (i = 0; i <= max; i++)
-        count[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		count[array[i]]++;
 
-    for (i = 0; i < (int)size; i++)
-        count[array[i]]++;
+	for (i = 1; i <= max; i++)
+		count[i] += count[i - 1];
 
-    for (i = 1; i <= max; i++)
-        count[i] += count[i - 1];
+	output = malloc(sizeof(int) * size);
+	if (output == NULL)
+	{
+		free(count);
+		return;
+	}
 
-    output = malloc(sizeof(int) * size);
-    if (output == NULL)
-    {
-        free(count);
-        return;
-    }
+	for (i = size - 1; i >= 0; i--)
+	{
+		output[count[array[i]] - 1] = array[i];
+		count[array[i]]--;
+	}
 
-    for (i = size - 1; i >= 0; i--)
-    {
-        output[count[array[i]] - 1] = array[i];
-        count[array[i]]--;
-    }
+	for (i = 0; i < (int)size; i++)
+		array[i] = output[i];
 
-    for (i = 0; i < (int)size; i++)
-        array[i] = output[i];
+	printf("Counting array: ");
+	for (i = 0; i <= max; i++)
+	{
+		if (i < max)
+			printf("%d, ", count[i]);
+		else
+			printf("%d\n", count[i]);
+	}
 
-    printf("Counting array: ");
-    for (i = 0; i <= max; i++)
-    {
-        if (i < max)
-            printf("%d, ", count[i]);
-        else
-            printf("%d\n", count[i]);
-    }
-
-    free(count);
-    free(output);
+	free(count);
+	free(output);
 }
