9a913e5ab813671df5a9ecee9b4209cf9297cd82
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-27 14:08:53 +0200

    Quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -26,9 +26,9 @@ void swap(int *x, int *y)
 int lomuto_partition(int *array, int low, int high)
 {
 	int pivot = array[high];
-	int i = low - 1;
+	int i = low - 1, j;
 
-	for (int j = low; j < high; j++)
+	for (j = low; j < high; j++)
 	{
 		if (array[j] < pivot)
 		{
