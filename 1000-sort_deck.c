c9b1474460f40c5f3183cf652c1cee38ea233ad3
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-28 12:09:43 +0200

    Dealer

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -8,22 +8,22 @@
  */
 int card_comp(const card_t *card)
 {
-    int value;
+	int value;
 
-    if (strcmp(card->value, "Ace") == 0)
-        value = 1;
-    else if (strcmp(card->value, "10") == 0)
-        value = 10;
-    else if (strcmp(card->value, "Jack") == 0)
-        value = 11;
-    else if (strcmp(card->value, "Queen") == 0)
-        value = 12;
-    else if (strcmp(card->value, "King") == 0)
-        value = 13;
-    else
-        value = atoi(card->value);
+	if (strcmp(card->value, "Ace") == 0)
+		value = 1;
+	else if (strcmp(card->value, "10") == 0)
+		value = 10;
+	else if (strcmp(card->value, "Jack") == 0)
+		value = 11;
+	else if (strcmp(card->value, "Queen") == 0)
+		value = 12;
+	else if (strcmp(card->value, "King") == 0)
+		value = 13;
+	else
+		value = atoi(card->value);
 
-    return (value + card->kind * 13);
+	return (value + card->kind * 13);
 }
 
 /**
@@ -34,19 +34,19 @@ int card_comp(const card_t *card)
  */
 void swap_cards(deck_node_t *a, deck_node_t *b)
 {
-    deck_node_t *prev_a = a->prev;
-    deck_node_t *next_b = b->next;
+	deck_node_t *prev_a = a->prev;
+	deck_node_t *next_b = b->next;
 
-    if (prev_a)
-        prev_a->next = b;
-    b->prev = prev_a;
+	if (prev_a)
+		prev_a->next = b;
+	b->prev = prev_a;
 
-    a->next = next_b;
-    if (next_b)
-        next_b->prev = a;
+	a->next = next_b;
+	if (next_b)
+		next_b->prev = a;
 
-    a->prev = b;
-    b->next = a;
+	a->prev = b;
+	b->next = a;
 }
 
 /**
@@ -56,21 +56,21 @@ void swap_cards(deck_node_t *a, deck_node_t *b)
  */
 void sort_deck(deck_node_t **deck)
 {
-    deck_node_t *current, *next;
+	deck_node_t *current, *next;
 
-    if (!deck || !(*deck) || !((*deck)->next))
-        return;
+	if (!deck || !(*deck) || !((*deck)->next))
+		return;
 
-    current = (*deck)->next;
-    while (current)
-    {
-        next = current->next;
-        while (current->prev && card_comp(current->prev->card) > card_comp(current->card))
-        {
-            swap_cards(current->prev, current);
-            if (!current->prev)
-                *deck = current;
-        }
-        current = next;
-    }
+	current = (*deck)->next;
+	while (current)
+	{
+		next = current->next;
+		while (current->prev && card_comp(current->prev->card) > card_comp(current->card))
+		{
+			swap_cards(current->prev, current);
+			if (!current->prev)
+				*deck = current;
+		}
+		current = next;
+	}
 }
