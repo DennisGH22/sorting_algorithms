71f6e8f6a0e03cd5918b564922779d727271d588
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-28 10:34:06 +0200

    Quick Sort - Hoare Partition scheme

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -25,7 +25,7 @@ void swap(int *x, int *y)
 */
 int hoare_partition(int *array, int low, int high, size_t size)
 {
-	int pivot = array[low];
+	int pivot = array[high];
 	int i = low - 1;
 	int j = high + 1;
 
