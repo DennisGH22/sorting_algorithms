ddd49e9743d529484e1de896bd8fbb1f6f859174
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-28 13:25:57 +0200

    Bitonic sort

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,17 +1,18 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
- * swap_array - Swap two values in an array.
+ * swap - Swap two values in an array.
  *
- * @array: The array containing the values to swap.
- * @x: Index of the first item to swap.
- * @y: Index of the second item to swap.
-*/
-void swap_array(int array[], int x, int y)
+ * @arr: The array containing the values to swap.
+ * @item1: Index of the first item to swap.
+ * @item2: Index of the second item to swap.
+ */
+void swap(int arr[], int item1, int item2)
 {
-    int temp = array[x];
-    array[x] = array[y];
-    array[y] = temp;
+    int temp = arr[item1];
+    arr[item1] = arr[item2];
+    arr[item2] = temp;
 }
 
 /**
@@ -21,28 +22,34 @@ void swap_array(int array[], int x, int y)
  * @size: The size of the array.
  * @dir: Sorting direction (1 for ascending, 0 for descending).
  * @bitonic_size: Size of the subarrays to be merged.
-*/
+ */
 void bitonic_merge(int *array, size_t size, int dir, size_t bitonic_size)
 {
     size_t half, i;
-	int temp;
+    int temp;
 
     if (bitonic_size > 1)
     {
         half = bitonic_size / 2;
 
         if (dir == 1)
-            printf("Merging [%i/%i] (UP):\n", bitonic_size, size);
+        {
+            printf("Merging [%zu/%zu] (UP):\n", bitonic_size, size);
+            print_array(array, bitonic_size);
+        }
         else
-            printf("Merging [%i/%i] (DOWN):\n", bitonic_size, size);
-
-        print_array(array, bitonic_size);
+        {
+            printf("Merging [%zu/%zu] (DOWN):\n", bitonic_size, size);
+            print_array(array, bitonic_size);
+        }
 
         for (i = 0; i < size - half; i++)
         {
             if ((array[i] > array[i + half]) == dir)
             {
-                swap_array(array, i, i + half);
+                temp = array[i];
+                array[i] = array[i + half];
+                array[i + half] = temp;
             }
         }
 
@@ -50,11 +57,15 @@ void bitonic_merge(int *array, size_t size, int dir, size_t bitonic_size)
         bitonic_merge(array + half, half, dir, bitonic_size / 2);
 
         if (dir == 0)
-            printf("Result [%i/%i] (DOWN):\n", bitonic_size, size);
+        {
+            printf("Result [%zu/%zu] (DOWN):\n", bitonic_size, size);
+            print_array(array, bitonic_size);
+        }
         else
-            printf("Result [%i/%i] (UP):\n", bitonic_size, size);
-
-        print_array(array, bitonic_size);
+        {
+            printf("Result [%zu/%zu] (UP):\n", bitonic_size, size);
+            print_array(array, bitonic_size);
+        }
     }
 }
 
@@ -65,7 +76,7 @@ void bitonic_merge(int *array, size_t size, int dir, size_t bitonic_size)
  * @size: The size of the array.
  * @dir: Sorting direction (1 for ascending, 0 for descending).
  * @bitonic_size: Size of the subarrays.
-*/
+ */
 void bitonic_sort_recursive(int *array, size_t size, int dir, size_t bitonic_size)
 {
     size_t half;
@@ -85,7 +96,7 @@ void bitonic_sort_recursive(int *array, size_t size, int dir, size_t bitonic_siz
  *
  * @array: The array to be sorted.
  * @size: The size of the array.
-*/
+ */
 void bitonic_sort(int *array, size_t size)
 {
     if (array == NULL || size <= 1)
