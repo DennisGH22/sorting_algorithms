d6ac510a56b318cc087b037947a4a0441eb98b05
Tebogo Ndaba <dennis@immorfinite.co.za>
2023-09-28 11:17:12 +0200

    Quick Sort - Hoare Partition scheme

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -44,7 +44,7 @@ int hoare_partition(int *array, int low, int high, size_t size)
 		if (i >= j)
 			return (j);
 
-		swap(&array[i], &array[j]);
+		swap(&array[i], pivot);
 		print_array(array, size);
 	}
 }
@@ -65,7 +65,7 @@ void quicksort_hoare_recursive(int *array, int low, int high, size_t size)
 	{
 		pivot_idx = hoare_partition(array, low, high, size);
 
-		quicksort_hoare_recursive(array, low, pivot_idx - 1, size);
+		quicksort_hoare_recursive(array, low, pivot_idx, size);
 		quicksort_hoare_recursive(array, pivot_idx + 1, high, size);
 	}
 }
